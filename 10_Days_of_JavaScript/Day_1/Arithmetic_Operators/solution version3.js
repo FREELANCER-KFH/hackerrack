eed1348c8493ef9cc8c691efdacfb3a7da1f94b9
programmerKFH <kfh.freelancer@gmail.com>
2024-01-05 05:00:01 -0400

    Uploaded solution version3.js

---
+++ 10_Days_of_JavaScript/Day_1/Arithmetic_Operators/solution version3.js
@@ -0,0 +1,22 @@
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+
+// Write your code here
+const getArea = (length, width) => length * width
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+// Write your code here
+const getPerimeter = (length, width) => 2*(length+width)
