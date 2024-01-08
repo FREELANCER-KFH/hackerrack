2e9cfb0d823e4ddf4085bba7d002c9eca202aef6
programmerKFH <kfh.freelancer@gmail.com>
2024-01-08 01:08:26 -0400

    Uploaded differents ways to solve if-else challenge on day 2 of 10 days of JavaScript.

---
+++ 10_Days_of_JavaScript/Day_2/If-Else/solution version3.js
@@ -0,0 +1,17 @@
+const getGrade = (score) => {
+    const gradeDictionary = ['F','E','D','C','B','A']
+    const scoreDictionary = [0, 5,10,15,20,25,30]
+    
+    if (score < 0 || score > 30) {
+        return 'Invalid score! The score has to be between 0 and 30.'
+    } else {
+        for (let i = 5; i >= 0; i--) {
+            if (score > scoreDictionary[i] && score <= scoreDictionary[i+1]) {
+                return gradeDictionary[i]
+            }
+        }
+        return 'F'
+    }
+}
+
+getGrade(11) // D
---
+++ 10_Days_of_JavaScript/Day_2/If-Else/solution version5.js
@@ -0,0 +1,23 @@
+const getGrade = (score) => {
+    const gradeScoreMap = {
+        'A': 30,
+        'B': 25,
+        'C': 20,
+        'D': 15,
+        'E': 10,
+        'F': 5
+    };
+
+    if (score < 0 || score > 30) {
+        return 'Invalid score! The score has to be between 0 and 30.'
+    } else {
+        for (let grade in gradeScoreMap) {
+            if (score >= gradeScoreMap[grade] -4) {
+                return grade;
+            }
+        }
+    }
+    return 'F';
+}
+
+getGrade(11) // D
